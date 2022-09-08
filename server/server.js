58479fe2acfbf3f1fa61a16285fe5f7edfee3ff5
Nturn94 <Nathanturnbull@live.com>
2022-09-09 03:52:24 +1000

    More routes added. Missing create new channel and delete channel

--- server/server.js
+++ server/server.js
@@ -20,15 +20,13 @@ sockets.connect(io, PORT);
 server.listen(http, PORT);
 
 
-
-
-
-
 app.use(express.urlencoded({extended: true}));
 app.use(express.json())
+// app.use(bodyParser.json());
 
 const path = require('path');
 const { SystemJsNgModuleLoader } = require('@angular/core');
+const { stringify } = require('querystring');
 
 
 app.use(express.static(__dirname + "../dist/week4/"));
@@ -40,13 +38,14 @@ app.all("/*", function(req, res, next){
     next();
   });
 
-let ksu = {username : 'k@s', pwd: '1', userid: '1', userbirthdate: '1900', userage: '122', groups: ["groupa"]};
-let Ntl = {username : 'n@t', pwd: '2', userid: '2' , userbirthdate: '', userage: '', groups: ["groupa"]};
-let Abn = {username : 'a@b', pwd: '3', userid: '3', userbirthdate: '', userage: '', groups: ["groupa", "groupc"]};
+let ksu = {username : 'k@s', pwd: '1', userid: '1', userbirthdate: '1900', userage: '122', groups: ["groupa"], adminstatus: "yes", GroupAssis: ["groupa", "groupb", "groupc"], channels: ["channel1"]};
+let Ntl = {username : 'n@t', pwd: '2', userid: '2' , userbirthdate: '', userage: '', groups: ["groupa"], adminstatus: "no", GroupAssis: ["groupa"], channels: ["channel1"]};
+let Abn = {username : 'a@b', pwd: '3', userid: '3', userbirthdate: '', userage: '', groups: ["groupa", "groupc"], adminstatus: "no", GroupAssis: ["groupa"], channels: ["channel1"]};
 
-let groups = ["groupa", "groupb", "groupc"];
+const groups = new Array ("groupa", "groupb", "groupc");
 let users = [ksu, Ntl, Abn];
-
+let newusers = [];
+const channels = new Array ("channel1", "channel2", "channel3");
 app.post('/ping', function (req, res) {
     console.log("hello", req.body)
     state = false;
@@ -88,21 +87,69 @@ app.post('/ping', function (req, res) {
   })
 
 app.post('/newgroup', async (req, res) =>{
-    // console.log("hello", req.body)
+    const buffers = [];
+    for await (const chunk of req) {
+      buffers.push(chunk);
+    }
+    const data = Buffer.concat(buffers).toString();
+    this.groups = JSON.stringify(this.groups);
+    this.groups += data;
+    console.log(this.groups);
 
-    console.log("hello");
-    res.status(200).json("data");
-    
+});
+app.post('/newuser', async (req, res) =>{
+    const buffers = [];
+    for await (const chunk of req) {
+      buffers.push(chunk);
+    }
+    const data = Buffer.concat(buffers).toString();
+    let index = this.newusers.indexOf(data);
+    if(index===-1){
+        this.newusers = JSON.stringify(this.newusers);
+        this.newusers += data;
+        console.log(this.newusers);
 
+    }else{
+        console.log("User already exists");
+    }
+
+});
+app.post('/deluser', async (req, res) =>{
+    const buffers = [];
+    for await (const chunk of req) {
+      buffers.push(chunk);
+    }
+    const data = Buffer.concat(buffers).toString();
+    let index = this.newusers.indexOf(data);
+    newusers = newusers.replaceAt(index, '');
+    console.log(this.newusers);
+});
+app.post('/delgroup', async (req, res) =>{
+    const buffers = [];
+    for await (const chunk of req) {
+      buffers.push(chunk);
+    }
+    const data = Buffer.concat(buffers).toString();
+    let index = this.groups.indexOf(data);
+    groups = groups.replaceAt(index, '');
+    console.log(this.groups);
 });
 
 app.get("/rtv", function (req, res) {
-    res.status(200).json({groups: groups, users: users });
+    res.status(200).json({groups: groups, users: users, channels: channels });
 });
 
 app.post('/assign', async (req, res) =>{
-    console.log(req.body);
-    const myArray = req.body.split(" ");
+
+    const buffers = [];
+
+    for await (const chunk of req) {
+      buffers.push(chunk);
+    }
+  
+    const data = Buffer.concat(buffers).toString();
+    console.log(data);
+    const myArray = data.split(" ");
 
     if(CheckEqual(ksu, myArray[1])===true){
         if (this.ksu.groups.indexOf(myArray[0])===-1){
@@ -129,77 +176,80 @@ app.post('/assign', async (req, res) =>{
         }
     }
 
-    //group, user
+
 
 });
+app.post('/assignchannel', async (req, res) =>{
+
+    const buffers = [];
+
+    for await (const chunk of req) {
+      buffers.push(chunk);
+    }
+  
+    const data = Buffer.concat(buffers).toString();
+    console.log(data);
+    const myArray = data.split(" ");
+
+    if(CheckEqual(ksu, myArray[1])===true){
+        if (this.ksu.channels.indexOf(myArray[0])===-1){
+            this.ksu.channels.push(myArray[0]);
+            console.log("User added to channel!");
+        }
+    }else if(CheckEqual(Ntl, myArray[1])===true){
+        if (this.Ntl.channels.indexOf(myArray[0])===-1){
+            this.Ntl.channels.push(myArray[0]);
+            console.log("User added to channel!");
+        }
+    }else if(CheckEqual(Abn, myArray[1])===true){
+        if (this.Abn.channels.indexOf(myArray[0])===-1){
+            this.Abn.channels.push(myArray[0]);
+            console.log("User added to channel!");
+        }
+
+    }
+
+    function CheckEqual(object1, object2) {
+        if(object1.username === object2){
+            authcheck = true;
+            return(authcheck);
+        }
+    }
 
 
-// res.send({data: dataChunk, results: results[1]});
 
-// app.get("/rtv", function (req, res) {
-//     res.status(200).json({ status: "UP" });
-// });
-// _________________________________________________
+});
+app.post('/newchannel', async (req, res) =>{
+    const buffers = [];
+    for await (const chunk of req) {
+      buffers.push(chunk);
+    }
+    const data = Buffer.concat(buffers).toString();
+    let index = this.channels.indexOf(data);
+    if(index===-1){
+        this.channels = JSON.stringify(this.channels);
+        this.channels += data;
+        console.log(this.channels);
 
-// app.listen(3000,()=>{
-//     var d = new Date();
-//     var n = d.getHours();
-//     var m = d.getMinutes();
-//     console.log("server has been started at : "+n+":"+m);
-// });
+    }else{
+        console.log("User already exists");
+    }
 
-
-
-
-
-
-// app.get("/task2", function (req, res) {
-//     res.sendFile(__dirname + "/www/task2.html");
-//     });
-
-// app.get("/test", function (req, res) {
-//     res.sendFile(__dirname + "/www/test.html");
-//     });
-
-app.post('/login', require('./router/postLogin'));
-app.post('/loginafter', require('./router/postLoginafter'));
+});
+app.post('/delchannel', async (req, res) =>{
+    const buffers = [];
+    for await (const chunk of req) {
+      buffers.push(chunk);
+    }
+    const data = Buffer.concat(buffers).toString();
+    let index = this.channels.indexOf(data);
+    channels = channels.replaceAt(index, '');
+    console.log(this.channels);
+});
 
 
 app.get("/", function (req, res) {
     res.sendFile(__dirname + "../dist/week4/");
     });
 
-// app.get("/endpoint", function (req, res) {
-//         res.sendFile(__dirname + "/www/endpoint.html");
-//         });
 
-app.post('/api/login', function(req, res){
-
-    let users = [{'email' : 'abc@com.au', 'pwd' : '123'}, {'email' : 'abd@com.au', 'pwd' : '123'}, {'email' : 'abe@com.au', 'pwd': '123'}]
-
-    if (!req.body) {
-        return res.sendStatus(400)
-    }
-
-
-    var customer = {};
-    customer.email = req.body.email;
-    customer.upwd = req.body.upwd;
-    customer.valid = false;
-
-    for (let i=0; i<users.length;i++){
-        if (req.body.email == users[i].email && req.body.upwd == users[i].pwd){
-            customer.valid = true;
-        }
-    }
-
-    // if (req.body.email == "abc@com.au" && req.body.upwd == "123"){
-    //     customer.valid = true;
-    // }else{
-    //     customer.valid = false;
-    // }
-
-        
-    res.send(customer);
-
-});
--- src/app/msp/msp.component.ts
+++ src/app/msp/msp.component.ts
@@ -25,9 +25,44 @@ export class MSPComponent implements OnInit {
   users: any=[];
   userlist:string="";
   tempuser = Array();
+  tempchannel = Array();
   groups: any = [];
   grouplist:string="";
   newroomname:string="";
+  newusername:string="";
+  newuserpwd:string="";
+  delusersel:string="";
+  delgroupsel:string="";
+  channellist:string="";
+  newchannelname:string="";
+  channels:any =[];
+  isdiv1 = false;
+  isdiv2 = false;
+  isdiv3 = false;
+  isdiv4 = false;
+  togglediv1(){
+    this.isdiv2 = false;
+    this.isdiv3 = false;
+    this.isdiv1 = true;
+  } 
+  togglediv2(){
+    this.isdiv2 = true;
+    this.isdiv3 = false;
+    this.isdiv1 = false;
+  } 
+  togglediv3(){
+    this.isdiv2 = false;
+    this.isdiv3 = true;
+    this.isdiv1 = false;
+  } 
+  togglediv4(){
+    this.isdiv2 = false;
+    this.isdiv3 = false;
+    this.isdiv1 = false;
+    this.isdiv4 = true;
+  } 
+
+
 
   
 getStatus(): Promise<void | any> {
@@ -61,14 +96,18 @@ private error (error: any) {
       }
       this.groups = result.groups;
       this.users = this.tempuser;
+      this.channels= result.channels;
 
 
     });
+    
 
 }
 
-AssignUser(){
 
+AssignUser(){
+  const headers = new HttpHeaders()
+  headers.append('Content-Type', 'application/json; charset=utf-8');
 
   var sendgroup = this.grouplist;
   var senduser = this.userlist;
@@ -76,20 +115,97 @@ AssignUser(){
 
   console.log(combination);
 
-  this.http.post('http://127.0.0.1:3000/assign', JSON.stringify(combination), {
-})
+  return this.http.post('http://127.0.0.1:3000/assign', JSON.stringify(combination), {
+    headers: headers
+}) .subscribe(data => {
+  console.log(data);
+});
 
 }
 
 PostNewGroup(){
 
+  const headers = new HttpHeaders()
+  headers.append('Content-Type', 'application/json; charset=utf-8');
+
+
   console.log("New room name is:", this.newroomname);
 
-  this.http.post('http://127.0.0.1:3000/newgroup', JSON.stringify(this.newroomname), {
-  // headers: headers
+  return this.http.post('http://127.0.0.1:3000/newgroup', JSON.stringify(this.newroomname), {
+  headers: headers
 })    .subscribe(data => {
   console.log(data);
 });
+
+}
+PostNewUser(){
+
+  const headers = new HttpHeaders()
+  headers.append('Content-Type', 'application/json; charset=utf-8');
+
+
+  console.log("New user name is:", this.newusername+this.newuserpwd);
+  var newuser = this.newusername+this.newuserpwd;
+
+  return this.http.post('http://127.0.0.1:3000/newuser', JSON.stringify(newuser), {
+  headers: headers
+})    .subscribe(data => {
+  console.log(data);
+});
+
+}
+DelUser(){
+
+  const headers = new HttpHeaders()
+  headers.append('Content-Type', 'application/json; charset=utf-8');
+
+
+  console.log("This user is deleted", this.delusersel);
+
+  return this.http.post('http://127.0.0.1:3000/deluser', JSON.stringify(this.delusersel), {
+  headers: headers
+})    .subscribe(data => {
+  console.log(data);
+});
+
+}
+
+delgroup(){
+  
+  const headers = new HttpHeaders()
+  headers.append('Content-Type', 'application/json; charset=utf-8');
+
+
+  console.log("This group is deleted", this.delgroupsel);
+
+  return this.http.post('http://127.0.0.1:3000/delgroup', JSON.stringify(this.delgroupsel), {
+  headers: headers
+})    .subscribe(data => {
+  console.log(data);
+});
+
+}
+AssignUserToChannel(){
+  const headers = new HttpHeaders()
+  headers.append('Content-Type', 'application/json; charset=utf-8');
+
+  var sendchannel = this.channellist;
+  var senduser = this.userlist;
+  var combination = sendchannel+" "+senduser;
+
+  console.log(combination);
+
+  return this.http.post('http://127.0.0.1:3000/assignchannel', JSON.stringify(combination), {
+    headers: headers
+}) .subscribe(data => {
+  console.log(data);
+});
+
+}
+PostNewChannel(){ //unfinished
+
+}
+delChannel(){ //unfinished
 
 }
 
